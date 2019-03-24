1367254219d438c1f01e1e0af1f8dbb5941d565c
Paul cartau <pcartau@student.42.fr>
2019-03-24 14:13:08 +0100

    Ajout de la fonction rematch
    -Rematch de chats
    -Add event sur images

--- public/js/matching.js
+++ public/js/matching.js
@@ -1,3 +1,4 @@
+/*---------------------------DOM-CREATION------------------------------------*/
 var catsData = JSON.parse(catsData.replace(/&quot;/g,'"'));
 var cat1 = document.getElementById("cat1");
 var cat2 = document.getElementById("cat2");
@@ -5,9 +6,33 @@ var cat1_img = document.createElement("img");
 var cat2_img = document.createElement("img");
 
 catsMatchs = catsMatchs.split(',');
-var rand = Math.floor(Math.random() * catsMatchs.length);
+//Afin de lire les matchs
 
-cat1_img.src = catsData[catsMatchs[rand].split(':')[0]].cat_url;
+var randomMatch = Math.floor(Math.random() * catsMatchs.length);
+//Choix d'un match au hasard
+
+cat1_img.src = catsData[catsMatchs[randomMatch].split(':')[0]].cat_url;
 cat1.appendChild(cat1_img);
-cat2_img.src = catsData[catsMatchs[rand].split(':')[1]].cat_url;
+cat2_img.src = catsData[catsMatchs[randomMatch].split(':')[1]].cat_url;
 cat2.appendChild(cat2_img);
+
+
+/*---------------------------REMATCH-FUNCTION---------------------------------*/
+function rematch() {
+  catsMatchs.splice(randomMatch, 1); //Retrait du match
+  randomMatch = Math.floor(Math.random() * catsMatchs.length);
+  cat1_img.src = catsData[catsMatchs[randomMatch].split(':')[0]].cat_url;
+  cat1.appendChild(cat1_img);
+  cat2_img.src = catsData[catsMatchs[randomMatch].split(':')[1]].cat_url;
+  cat2.appendChild(cat2_img);
+}
+
+
+/*---------------------------EVENTS-ON-IMGS---------------------------------*/
+cat1.addEventListener('click', function() {
+  rematch();
+});
+
+cat2.addEventListener('click', function() {
+  rematch();
+});
