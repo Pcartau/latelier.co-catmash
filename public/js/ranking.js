3c092cfaf7d31351399086d5484c24154466bc89
Paul cartau <pcartau@student.42.fr>
2019-03-25 11:35:30 +0100

    Ajout variables ranking
    -Votes
    -Ranking

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -1,5 +1,6 @@
 const img_container = document.getElementsByClassName('img_container')[0];
 var catsData = JSON.parse(catsData.replace(/&quot;/g,'"'));
+var catRank = 1;
 
 var catsSorted = catsData.sort(function (cat_1, cat_2) {
     return cat_2.cat_votes - cat_1.cat_votes;
@@ -8,11 +9,18 @@ var catsSorted = catsData.sort(function (cat_1, cat_2) {
 
 for (let cat of catsSorted) {
   var img = document.createElement('img');
-  var p = document.createElement('p');
+  var pVotes = document.createElement('p');
+  var pRank = document.createElement('p');
+
+  pRank.classList.add("cat_rank");
+  pVotes.classList.add("cat_votes");
   img.src = cat.cat_url;
   img.classList.add("cat_img");
-  p.innerHTML = cat.cat_votes;
+  pRank.innerHTML = "#" + catRank;
+  pVotes.innerHTML = cat.cat_votes + " vote(s)";
   img_container.appendChild(img);
-  img_container.appendChild(p);
+  img_container.appendChild(pRank);
+  img_container.appendChild(pVotes);
+  catRank += 1;
 }
 //Remplissage dynamique de la div conteneuse des images
