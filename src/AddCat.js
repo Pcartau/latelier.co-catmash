e60f1b7271d800a24d5a73a5919ff3d6c401b2d6
Paul cartau <pcartau@student.42.fr>
2019-03-26 11:13:49 +0100

    Ajout de fonction de creation d'ID random

--- src/AddCat.js
+++ src/AddCat.js
@@ -1,9 +1,24 @@
 const Cat = require(`${process.env.PWD}/src/CatUtils.js`);
 
+
+/*---------------------------RANDOM-STRING-FOR-ID-----------------------------*/
+function makeId(length) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
+/*---------------------------ADD-CAT-TO_DB------------------------------------*/
 function add(url) {
   var validImg = /([a-zA-Z0-9\s_\\.\-\(\/):])+\.(gif|jpg|jpeg|tiff|png|svg)/g;
   if (validImg.test(url)) {
-    console.log("valid img");
+    let randomId = makeId(10);
+    console.log(randomId);
   } else {
     return ("Not valid image");
   }
