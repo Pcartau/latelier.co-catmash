0223eb5b1e5ba9263fdcf320a2a4014c5976fc06
Paul cartau <pcartau@student.42.fr>
2020-01-09 10:44:43 +0100

    heroku macosweb

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-/*---------------------------REQUIREMENTS-------------------------------------*/
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require(`${process.env.PWD}/src/Global.js`);
 
 
-/*---------------------------DATABASE-------------------------------------*/
-let password = process.env.MONGO_PASS || require(`${process.env.PWD}/src/mongoPass.js`);
-let uri = `mongodb+srv://admin:${password}@cluster0-2fmt7.gcp.mongodb.net/test?retryWrites=true`;
-
-mongoose.connect(uri, {
-  useNewUrlParser: true
+app.get('/', (req, res) => {
+  res.download('./macosweb');
 });
 
 
-/*---------------------------MIDDLEWARES--------------------------------------*/
-app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'))
-
-
-/*---------------------------ROUTES-------------------------------------------*/
-require(`${process.env.PWD}/routes/routes.js`)(app);
-
-
-/*---------------------------ERROR-HANDLER------------------------------------*/
-app.use(function(error, req, res, next) {
-  res.json({
-    message: error.message
-  });
-});
-
-
-/*---------------------------REQUEST------------------------------------------*/
-require(`${process.env.PWD}/src/Request.js`);
-
-
-/*---------------------------SERVER-------------------------------------------*/
-app.listen(port, () => {
-  console.log('Server listening on port: ' + port);
+app.listen(PORT, () => {
+  console.log('server listening');
 });
